de2356afa57a358e7436e776712b3875200c8604
honda <honda@rock79.csse.muroran-it.ac.jp>
2021-07-28 13:09:28 +0900

    data_collection size

--- data_collection.py
+++ data_collection.py
@@ -7,6 +7,8 @@ import numpy as np
 import csv
 import os
 
+PERIOD=0.1
+
 udp = sk.UDP_Recv(sk.learning_machine,sk.sensor_port)
 data = [0]
 teacher_data_list = []
@@ -14,14 +16,27 @@ teacher_data_list = []
 key = keyin.Keyboard()
 ch = 'c'
 data_number = 0
+now=time.time()
+start=now
+init=now
+count=0
+print("Input 'q' to stop.")
 while ch !='q':
     try:
         data = udp.recv()
         data_number = data_number + 1
         teacher_data_list.append(data)
-        print("data got : ",data_number)
     except (BlockingIOError,socket.error):
         time.sleep(0.0001)
+
+    if now-init>PERIOD:
+        rate=count/PERIOD
+        print("\r %5.2f %5.2f %d" % (now-start,rate,data_number),end='')
+        count=0
+        init=now
+
+    count+=1
+    now=time.time()
     ch = key.read()
 
 f = open('chainer_data_in.csv','w',encoding='utf-8')
