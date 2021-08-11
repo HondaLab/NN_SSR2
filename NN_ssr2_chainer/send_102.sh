bc7343658fc3f5c79d4243978c621d571770778b
lifangzheng333 <67769865+lifangzheng333@users.noreply.github.com>
2021-08-11 02:05:40 -0700

    Delete send_102.sh

--- NN_ssr2_chainer/send_102.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/expect
-
-set timeout 9
-spawn scp data_in_max.csv data_out_max.csv optimum_weight_1000 pi@172.16.7.102:/home/pi/source_code_version3
-expect "s password:"
-send "ssr2\n"
-interact
