1f66d9739e8781530101780d03457759b0f5ac77
lifangzheng333 <67769865+lifangzheng333@users.noreply.github.com>
2021-11-09 03:27:33 -0800

    Update nn_ssr2_h1.py

--- NN_ssr2_chainer/nn_ssr2_h1.py
+++ NN_ssr2_chainer/nn_ssr2_h1.py
@@ -20,7 +20,7 @@ OUT_FILE="robot_perspective_flim.mp4"
 fmt = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
 record_fps=10
 width=320
-height=70
+height= ic.im_cut_below - ic.im_cut_up
 print("# Resolution: %5d x %5d" % (width,height))
 size = (width, height)
 vw = cv2.VideoWriter(OUT_FILE, fmt, record_fps, size)
