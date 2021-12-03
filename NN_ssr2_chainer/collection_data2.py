4624c8bc1b19cb687e7f290c0cdb698184378ad0
Yasushi Honda <aeroboy08@gmail.com>
2021-12-04 01:28:43 +0900

    show frame imediatly

--- NN_ssr2_chainer/collection_data2.py
+++ NN_ssr2_chainer/collection_data2.py
@@ -32,13 +32,9 @@ def Run(mL,mR,left,right):
     mL.run(left)
     mR.run(right)
 
-def send_data(l,r,vw):
-    camera.cam.capture(camera.rawCapture, format="bgr", use_video_port=True)
-    frame = camera.rawCapture.array
+def send_data(frame,l,r,vw):
     frame2 = frame[ic.im_cut_up:ic.im_cut_below,:,:]
     vw.write(frame2)
-    cv2.imshow('frame',frame2)
-    cv2.waitKey(1)
     for i in range(0,camera.RES_X):
        picture_data.append(sum(frame[ic.im_cut_up:ic.im_cut_below,i,0]))
     for i in range(0,camera.RES_X):
@@ -67,17 +63,16 @@ def send_data(l,r,vw):
     picture_data.append(r)
     udp_send_data.send(picture_data)
     picture_data.clear()
-    camera.rawCapture.truncate(0)
 
 if __name__=="__main__":
     OUT_FILE="data_collection_output.avi"
     fmt = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
     record_fps=10
-    width=320
-    height= ic.im_cut_below - ic.im_cut_up
-    print("# Resolution: %5d x %5d" % (width,height))
-    size = (width, height)
-    vw = cv2.VideoWriter(OUT_FILE, fmt, record_fps, size)
+    crop_width=320
+    crop_height= ic.im_cut_below - ic.im_cut_up
+    print("\n# Resolution: %5d x %5d" % (crop_width,crop_height))
+    crop_size = (crop_width, crop_height)
+    vw = cv2.VideoWriter(OUT_FILE, fmt, record_fps, crop_size)
     udp_send_data = sk.UDP_Send(sk.data_reciving_terminal,sk.sensor_port)
    
     STEP = 35
@@ -95,56 +90,69 @@ if __name__=="__main__":
    
     key = keyin.Keyboard()
     ch="c"
-    send_start = "stop"
     PERIOD=0.2
     now = time.time()
     start = now
     init = now
     count=0
-    print("Input q to stop.")
+    send_data_count=0
+    print("# Input q to stop.")
     left=0
     right=0
-    send_num = 0
+    print("#   time,left,right,rate,send_data")
     while ch!="q":
-        try:
-            if ch == "w" :
-                left += STEP
-                right += STEP
-                send_data(left,right,vw)
-                Run(mL,mR,left,right)
-            if ch == "s" :
-                left -= STEP
-                right -= STEP
-                send_data(left,right,vw)
-                Run(mL,mR,left,right)
+        try: # KeyboardInterrupt
+            try: # cam.capture
+               camera.cam.capture(camera.rawCapture, format="bgr", use_video_port=True)
+               frame = camera.rawCapture.array
+               cv2.imshow('frame',frame)
+               cv2.waitKey(1)
 
-            if ch == "a" :
-                left = -(STEP*1.6)
-                right = -(STEP*1.6)
-                send_data(left,right,vw)
-                Run(mL,mR,left,right)
-            if ch == "d" :
-                left = 0
-                right = 0
-                Run(mL,mR,left,right)
+               if ch == "w" :
+                  left += STEP
+                  right += STEP
+                  send_data(frame,left,right,vw)
+                  send_data_count+=1
+                  Run(mL,mR,left,right)
+               if ch == "s" :
+                  left -= STEP
+                  right -= STEP
+                  send_data(frame,left,right,vw)
+                  send_data_count+=1
+                  Run(mL,mR,left,right)
+               if ch == "a" :
+                  left = -(STEP*1.6)
+                  right = -(STEP*1.6)
+                  send_data(frame,left,right,vw)
+                  send_data_count+=1
+                  Run(mL,mR,left,right)
+               if ch == "d" :
+                  left = 0
+                  right = 0
+                  Run(mL,mR,left,right)
+               if ch == "j" :
+                  left = left - HANDLE_STEP
+                  left_flag = left_flag - HANDLE_STEP
+                  send_data(frame,left,right,vw)
+                  send_data_count+=1
+                  Run(mL,mR,left,right)
+               if ch == "k" :
+                  right = right - right_flag
+                  left = left - left_flag
+                  right_flag = 0
+                  left_flag = 0
+                  send_data(frame,left,right,vw)
+                  send_data_count+=1
+                  Run(mL,mR,left,right)
+               if ch == "l" :
+                  right = right - HANDLE_STEP
+                  right_flag = right_flag - HANDLE_STEP
+                  send_data(frame,left,right,vw)
+                  send_data_count+=1
+                  Run(mL,mR,left,right)
 
-            if ch == "j" :
-                left = left - HANDLE_STEP
-                left_flag = left_flag - HANDLE_STEP
-                send_data(left,right,vw)
-                Run(mL,mR,left,right)
-            if ch == "k" :
-                right = right - right_flag
-                left = left - left_flag
-                right_flag = 0
-                left_flag = 0
-                send_data(left,right,vw)
-                Run(mL,mR,left,right)
-            if ch == "l" :
-                right = right - HANDLE_STEP
-                right_flag = right_flag - HANDLE_STEP
-                send_data(left,right,vw)
-                Run(mL,mR,left,right)
+            except:
+               pass
                 
         except KeyboardInterrupt:
             mL.run(0)
@@ -153,11 +161,13 @@ if __name__=="__main__":
 
         now=time.time()
         if now-start>PERIOD:        
-           print("\r %7.1f %4d %4d" % (now-init,left,right),end='')
+           rate=int(count/PERIOD)
+           print("\r %7.1f %4d %4d %5d %5d" % (now-init,left,right,rate,send_data_count),end='')
            count=0
            start=now
            
         count+=1
+        camera.rawCapture.truncate(0)
         ch = key.read()
 
     print("\n See your again!")
