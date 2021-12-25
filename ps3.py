f5d815d02a31e4e9afb857e6d060dc7ce11a5257
Y.Honda <aeroboy08@gmail.com>
2021-12-26 04:18:33 +0900

    calc and show send rate

--- ps3.py
+++ ps3.py
@@ -2,9 +2,12 @@
 # Yasushi Honda 2021 12/23
 # PS3のジョイスティック値を読み込んで、raspi にソケットで送る。
 import pygame
+import time
 from pygame.locals import *
 import modules.socket as sk
 import modules.keyin as kin
+
+PERIOD=0.5
 
 # ジョイスティックの初期化
 pygame.joystick.init()
@@ -26,29 +29,28 @@ pygame.init()
 screen = pygame.display.set_mode((320, 320))
 
 message="ジョイステックの値を %s に送信します"
-print(message % sk.robot)
-udp=sk.UDP_Send(sk.robot,sk.port)
+print(message % sk.robot_addr)
+udp=sk.UDP_Send(sk.robot_addr,sk.robot_port)
 data=[]
 key=kin.Keyboard()
-active = True
+now=time.time()
+start=now
+init=now
 count=0
 ch='c'
 while ch!='q':
+   now=time.time()
    # イベントの取得
    for e in pygame.event.get():
-       # 終了ボタン
-       if e.type == QUIT:
-           active = False
 
        # ジョイスティックのボタンの入力
        if e.type == pygame.locals.JOYAXISMOTION:
-           count+=1
-           #print(count, joystick.get_axis(0), joystick.get_axis(1))
-           print("\r %d %10.7f %10.7f %10.7f %10.7f" % (count, joystick.get_axis(0), joystick.get_axis(1), joystick.get_axis(3), joystick.get_axis(4)),end='')
+           print("\r send rate:%3d Lx:%8.5f Ly:%8.5f Rx:%8.5f Ry:%8.5f" % (count,joystick.get_axis(0),joystick.get_axis(1),joystick.get_axis(3),joystick.get_axis(4)), end='')
            data.append(joystick.get_axis(0))
            data.append(joystick.get_axis(1))
            data.append(joystick.get_axis(3))
            data.append(joystick.get_axis(4))
+           count+=1
            udp.send(data)
            data.clear()
        '''
@@ -57,6 +59,11 @@ while ch!='q':
        elif e.type == pygame.locals.JOYBUTTONUP:
            print('ボタン'+str(e.button)+'を離した')
        '''
+
+   if now-start>PERIOD:
+      rate=int(count/PERIOD)
+      count=0
+      start=now
 
    ch=key.read()
 
